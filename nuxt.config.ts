1d9f7ce3bbac651f27a647c9552abc879f80a794
Mubashir Rehman <malikextra7@gmail.com>
2024-05-28 14:13:32 +0500

    adding nvd-u library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
-  devtools: { enabled: true },
+  devtools: { enabled: false },
+  css: ["nvd-u/u-core.scss"],
+  components: [
+    { path: "components", pathPrefix: false },
+    { path: "node_modules/nvd-u/components", pathPrefix: false },
+  ],
 });
